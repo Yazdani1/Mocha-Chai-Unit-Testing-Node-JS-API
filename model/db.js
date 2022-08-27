7cac903f5059a73dc491b078da2fc229ffdcb5ba
Yazdani Chowdhury <yaz4dani@gmail.com>
2022-08-27 09:40:02 +0300

    new features

--- model/db.js
+++ model/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose.connect(
-  process.env.localdb,
+  process.env.MONGODBURL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
